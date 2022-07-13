499fdf4ccdb58ed161ed25d504cf27cbd725f36b
Felix Ye <felixyjs@gmail.com>
2022-07-13 19:01:42 -0400

    fix: fix a bug in ULProjectCover

--- frontend/src/components/ULProjectCover.js
+++ frontend/src/components/ULProjectCover.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Card } from "react-bootstrap";
 import defaultDocument from "../media/document.png";
 
-const ULProjectCover = () => {
+const ULProjectCover = (props) => {
   return (
     <Card className="col-2">
       <Card.Img variant="top" src={defaultDocument} />
       <Card.Body>
-        <Card.Title>{this.state.name}</Card.Title>
+        <Card.Title>{props.name}</Card.Title>
       </Card.Body>
     </Card>
   );
