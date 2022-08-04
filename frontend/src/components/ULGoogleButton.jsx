ad8b486b618f0159b708dd37ffe4f4b55c9732f2
chocolatefrog7 <61072834+chocolatefrog7@users.noreply.github.com>
2022-08-03 21:03:39 -0400

    change ui of signup

--- frontend/src/components/ULGoogleButton.jsx
+++ frontend/src/components/ULGoogleButton.jsx
@@ -35,31 +35,14 @@ export function ULGoogleButton() {
     google.accounts.id.renderButton(document.getElementById("signInDiv"), {
       theme: "outline",
       size: "large",
+      shape: "pill",
     });
   }, []);
 
   return (
     <div className="gsibutton">
+      <script src="https://accounts.google.com/gsi/client" async defer></script>
       <div id="signInDiv"></div>
     </div>
   );
 }
-
-// const refreshTokenSetup = response => {
-//   let refreshTiming = (response.tokenObj.expires_in || 3600 - 5 * 60) * 1000;
-
-//   const refreshToken = async () => {
-//   const newAuthRes = await response.reloadAuthResponse();
-//   refreshTiming = (newAuthRes.expires_in || 3600 - 5 * 60) * 1000;
-//   console.log('newAuthRes:', newAuthRes);
-//   // saveUserToken(newAuthRes.access_token);  <-- save new token
-//   localStorage.setItem('authToken', newAuthRes.id_token);
-
-//   // Setup the other timer after the first one
-//   setTimeout(refreshToken, refreshTiming);
-// };
-
-// // Setup first refresh timer
-// setTimeout(refreshToken, refreshTiming);
-// };
-//  refreshTokenSetup(response);
--- frontend/src/routes/ULSignUp.jsx
+++ frontend/src/routes/ULSignUp.jsx
@@ -1,6 +1,7 @@
 import { Link, useNavigate } from "react-router-dom";
 import Stack from "react-bootstrap/Stack";
 import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { ULSignUpForm } from "../components/ULSignUpForm";
@@ -26,19 +27,22 @@ export default function ULSignUp() {
   };
 
   return (
-    <Container>
-      <script src="https://accounts.google.com/gsi/client" async defer></script>
+    <Container className="justify-content-md-center" md={{ span: 5 }}>
+      <Card border="light">
       <Row className="justify-content-md-center">
-        <Col md={{ span: 6 }}>
+        <Col md={{ span: 4 }}>
           <Stack gap={3}>
-            Create an account to use The UI Lab
-            <ULGoogleButton />
-            or
+            <Card.Title>Create an account to use The UI Lab</Card.Title>
+            <Card.Body>
+              <ULGoogleButton />
+            <Card.Text>or</Card.Text>
             <ULSignUpForm createUser={createUser} />
             <Link to="/Login">Log in</Link>
+            </Card.Body>
           </Stack>
         </Col>
       </Row>
+      </Card>
     </Container>
   );
 }
